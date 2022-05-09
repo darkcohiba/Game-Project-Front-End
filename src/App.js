103c8c8c5f6495a4a357b6161e35034dbd734d3c
Sam <samuel.waters@gmail.com>
2022-05-09 12:10:00 -0600

    done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,62 @@
-// import styled from 'styled-components'
-// import DoodleJump from './views/DoodleJump.js'
-import './index.css'
-import FlappyBird from './views/FlappyBird.js'
+import React, { Component } from "react";
+import { HashRouter as Router, Route, NavLink } from "react-router-dom";
+import SignUpForm from "./SignUpForm";
+import SignInForm from "./SignInForm";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <FlappyBird/>
-        {/* <DoodleJump/> */}
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <Router basename="/react-auth-ui/">
+        <div className="App">
+          <div className="appAside" />
+          <div className="appForm">
+            <div className="pageSwitcher">
+              <NavLink
+                to="/sign-in"
+                activeClassName="pageSwitcherItem-active"
+                className="pageSwitcherItem"
+              >
+                Sign In
+              </NavLink>
+              <NavLink
+                exact
+                to="/"
+                activeClassName="pageSwitcherItem-active"
+                className="pageSwitcherItem"
+              >
+                Sign Up
+              </NavLink>
+            </div>
+
+            <div className="formTitle">
+              <NavLink
+                to="/sign-in"
+                activeClassName="formTitleLink-active"
+                className="formTitleLink"
+              >
+                Sign In
+              </NavLink>{" "}
+              or{" "}
+              <NavLink
+                exact
+                to="/"
+                activeClassName="formTitleLink-active"
+                className="formTitleLink"
+              >
+                Sign Up
+              </NavLink>
+            </div>
+            <Router>
+            <Route exact path="/" component={SignInForm} />
+            <Route path="/sign-up" component={SignUpForm} />
+            </Router>
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
