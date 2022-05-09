1880b106f94fd8c6b3f31bfaa7720253526ad719
AlGuise <98363807+AlGuise@users.noreply.github.com>
2022-05-09 14:11:55 -0600

    Home Page

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,32 +1,38 @@
 import Header from './Header';
 
+
 export default function Homepage() {
     return(
         <>
             <Header/>
-            <div className="flex">
+            <div className="grid grid-cols-2 grid-rows-2 gap-4">
                 <div id="snake-game-bg">
-                    <p>div</p>
-
-                </div>
-                <div id="flappy-bird-game-bg">
-                    <p>div</p>
-
+                    <img src='*' alt='Snake Game' />
                 </div>
                 <div>
-                    <h1>Snake Instructions</h1>
-                    <h2>Controls</h2>
+                    <img src="./assets/flappy-bird-sample.png" alt='Flappy Bird Game' />
+                </div>
+                <div className='text-center'>
+                    <h1 className='text-2xl font-bold'>Snake Instructions</h1>
+                    <br/>
+                    <h2 className='text-xl font-semibold'>Objective</h2>
+                    <p>Collect as many apples without running into the wall or yourself.</p>
+                    <br/>
+                    <h2 className='text-xl font-semibold'>Controls</h2>
                     <p>W - Up</p>
                     <p>S - Down</p>
                     <p>A - Left</p>
                     <p>D - Right</p>
-                    <p>Collect as many apples without running into the wall or yourself.</p>
+                    
                 </div>
-                <div>
-                    <h1>Flappy Bird Instructions</h1>
-                    <h2>Controls</h2>
-                    <p>Space/Left Click - Fly up</p>
+                <div className='text-center'>
+                    <h1 className='text-2xl font-bold'>Flappy Bird Instructions</h1>
+                    <br/>
+                    <h2 className='text-xl font-semibold'>Objective</h2>
                     <p>Your bird will drop automaticly, fly as long as you can while avoiding obstacles!</p>
+                    <br/>
+                    <h2 className='text-xl font-semibold'>Controls</h2>
+                    <p>Space/Left Click - Fly up</p>
                 </div>
             </div>
         </>
