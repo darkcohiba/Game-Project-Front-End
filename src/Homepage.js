6d5ca7e3a8b8c3ff79f2a6dd78f022ede49134c9
AlGuise <98363807+AlGuise@users.noreply.github.com>
2022-05-09 12:39:40 -0600

    Created Home Page

---
+++ src/Homepage.js
@@ -0,0 +1,30 @@
+export default function Homepage() {
+    return(
+        <>
+            <Header/>
+            <div className="flex">
+                <div id="snake-game-bg">
+
+                </div>
+                <div id="flappy-bird-game-bg">
+
+                </div>
+                <div>
+                    <h1>Snake Instructions</h1>
+                    <h2>Controls</h2>
+                    <p>W - Up</p>
+                    <p>S - Down</p>
+                    <p>A - Left</p>
+                    <p>D - Right</p>
+                    <p>Collect as many apples without running into the wall or yourself.</p>
+                </div>
+                <div>
+                    <h1>Flappy Bird Instructions</h1>
+                    <h2>Controls</h2>
+                    <p>Space/Left Click - Fly up</p>
+                    <p>Your bird will drop automaticly, fly as long as you can while avoiding obstacles!</p>
+                </div>
+            </div>
+        </>
+    )
+}
