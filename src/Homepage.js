3b9447d28c20d0e608fc32848ac2c4bc44a98192
Sam <samuel.waters@gmail.com>
2022-05-09 12:33:59 -0600

    form

---
+++ src/Homepage.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+import Header from './Header';
+
+
+function HomePage() {
+
+
+  return (
+    <div>
+        <Header />
+        <p>this is home</p>
+    </div>
+  )
+    
+}
+
+export default HomePage;
