47f4f25b4fc203ed7ea38d947630471c26091e42
Sam <samuel.waters@gmail.com>
2022-05-09 14:15:51 -0600

    desktop dont work

--- src/Login.js
+++ src/Login.js
@@ -13,7 +13,7 @@ function Login({setUser,setIsAuthenticated}) {
           username: username,
           password
       }
-     
+      console.log(user)     
       fetch(`/login`,{
         method:'POST',
         headers:{'Content-Type': 'application/json'},
